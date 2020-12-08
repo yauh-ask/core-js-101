82f407790dfbe9fe21c33e5937c0cd10c1086237
yauh-ask <yauheniya.askolkava@gmail.com>
2020-12-08 13:46:32 +0100

    feat: objects not all

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Returns the rectagle object with width and height parameters and getArea() method
  *
@@ -20,10 +19,13 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle (width, height) {
+  this.width = width
+  this.height = height
+  this.getArea = function calculateArea () {
+    return this.width * this.height
+  }
 }
-
 
 /**
  * Returns the JSON representation of specified object
@@ -35,10 +37,9 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON (obj) {
+  return JSON.stringify(obj)
 }
-
 
 /**
  * Returns the object of specified type from JSON representation
@@ -51,10 +52,15 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON (proto, json) {
+  const representationObject = Object.create(proto)
+  const objectFromJSON = JSON.parse(json)
+  const arrayWithKeys = Object.keys(objectFromJSON)
+  arrayWithKeys.forEach(key => {
+    representationObject[key] = objectFromJSON[key]
+  })
+  return representationObject
 }
-
 
 /**
  * Css selectors builder
@@ -111,39 +117,38 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement (/* value */) {
+    throw new Error('Not implemented')
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
-  },
-};
-
+  combine (/* selector1, combinator, selector2 */) {
+    throw new Error('Not implemented')
+  }
+}
 
 module.exports = {
   Rectangle,
   getJSON,
   fromJSON,
-  cssSelectorBuilder,
-};
+  cssSelectorBuilder
+}
